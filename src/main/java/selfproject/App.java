2517dc3676bf85ffdc999b365fac36f625daf002
Bogdan Sadolinskiy <Bogdan.Sadolinskiy@onseo.biz>
2019-07-19 16:43:50 +0300

    init

---
+++ src/main/java/selfproject/App.java
@@ -0,0 +1,18 @@
+package selfproject;
+
+import spark.Spark;
+
+public class App 
+{
+    public static void main( String[] args ) {
+        Spark.port(80);
+
+
+        Spark.get("/", (req, res) -> "{\"msg\":\"Hello, World!\"}");
+
+        Spark.get("/stop", (req, res) -> {
+            Spark.stop();
+            return "{\"msg\":\"Bye!\"}";
+        });
+    }
+}
